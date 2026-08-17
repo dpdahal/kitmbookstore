d11b65180b1a60b07fbb5ff4f2c484d25224ed96
dpdahal <dp50dahal@gmail.com>
2026-08-17 09:16:52 +0545

    project start

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once 'header.php';
+?>
+
+<h1>About Us</h1>
+<p>Welcome to our website! We are a company dedicated to providing the best products and services.</p>
+
+<?php 
+require_once 'footer.php';
+?>
---
+++ contact.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once 'header.php';
+?>
+
+<h1>Contact Us</h1>
+<p>Feel free to reach out to us with any questions or inquiries.</p>
+
+<?php 
+require_once 'footer.php';
+?>
